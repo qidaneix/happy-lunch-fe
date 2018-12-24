9802048187b27488415913d67a8702f499057d1b
xiaodabao <xiaojin1992@126.com>
2018-12-24 18:29:38 +0800

    梦里不知深似客

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <people-counting></people-counting>
+    <fireworks></fireworks>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import fireworks from './components/fireworks.vue';
+import peopleCounting from './components/people-counting.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    fireworks,
+    peopleCounting,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+html,
+body,
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
 }
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html,
+body,
+header {
+	background-color: #111116; /* Black */
+}
+
+body {
+  position: absolute;
+  color: white;
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+}
+
+.color-01 { color: #FF1461; } /* Red */
+.color-02 { color: #FF7C72; } /* Orange */
+.color-03 { color: #FBF38C; } /* Yellow */
+.color-04 { color: #A6FF8F; } /* Citrus */
+.color-05 { color: #18FF92; } /* Green */
+.color-06 { color: #1CE2B2; } /* Darkgreen */
+.color-07 { color: #5EF3FB; } /* Turquoise */
+.color-08 { color: #61C3FF; } /* Lightblue */
+.color-09 { color: #5A87FF; } /* Blue */
+.color-10 { color: #8453E3; } /* Purple */
+.color-11 { color: #C26EFF; } /* Lavender */
+.color-12 { color: #FB89FB; } /* Pink */
 </style>
---
+++ src/components/people-counting.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{`现在有${number}人在休息室`}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class extends Vue {
+  public number = 888;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
