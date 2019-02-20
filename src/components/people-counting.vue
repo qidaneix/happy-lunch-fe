dc261f255a3510e08fd28a851921e1ebd651a15b
xiaodabao <xiaojin1992@126.com>
2019-02-20 17:23:44 +0800

    无条件

--- src/components/people-counting.vue
+++ src/components/people-counting.vue
@@ -1,15 +1,47 @@
 <template>
   <div class="people-counting">
-    {{`现在有${number}人在休息室`}}
+    <span v-show="!isError && !isReady">
+      现在有多少人在休息室 (⊙o⊙)？
+    </span>
+    <span v-show="isError">
+      我也不知道 (ಥ_ಥ)
+    </span>
+    <span v-show="!isError && isReady">
+      {{`现在有${number}人在休息室 ( •̀∀•́ )`}}
+    </span>
   </div>
 </template>
 
 <script lang="ts">
+import axios from 'axios';
+import io from 'socket.io-client';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class extends Vue {
-  public number = 888;
+  public number = 0;
+  public isError = false;
+  public isReady = false;
+
+  public created() {
+    setTimeout(() => {
+      // 获取初始化数据
+      axios.get('/res').then((resp) => {
+        this.number = resp.data.result.personNum;
+        this.isReady = true;
+      }, () => {
+        this.isError = true;
+      });
+
+      // 主动推送
+      const socket = io();
+      socket.on('res', (res: string) => {
+        const msg = JSON.parse(res);
+        this.number = msg.result.personNum;
+        this.isReady = true;
+      });
+    }, 1500);
+  }
 }
 </script>
 
@@ -22,5 +54,12 @@ export default class extends Vue {
   height: 100vh;
   line-height: 100vh;
   text-align: center;
+  font-size: 4vw;
+}
+
+@media (max-width: 767.98px) {
+  .people-counting {
+    font-size: 6vw;
+  }
 }
 </style>
